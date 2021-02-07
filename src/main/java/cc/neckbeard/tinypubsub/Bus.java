47621cc6add05dbcaef1ed86a81cc63ecaea17db
nothub <48992448+nothub@users.noreply.github.com>
2021-02-07 12:35:15 +0100

    reg unreg for objects with sub fields

--- src/main/java/cc/neckbeard/tinypubsub/Bus.java
+++ src/main/java/cc/neckbeard/tinypubsub/Bus.java
@@ -2,14 +2,39 @@ package cc.neckbeard.tinypubsub;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.stream.Collectors;
 
 public class Bus {
 
     private final Map<Class<?>, ConcurrentSkipListSet<Sub>> subs = new ConcurrentHashMap<>();
     private final Object lock = new Object();
+
+    private static Set<Sub> getSubs(Object obj) {
+        return Arrays
+            .stream(obj.getClass().getFields())
+            .filter(field -> field.getType().equals(Sub.class))
+            .map(field -> {
+                boolean access = field.isAccessible();
+                field.setAccessible(true);
+                try {
+                    return field.get(0);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } finally {
+                    field.setAccessible(access);
+                }
+                return null;
+            })
+            .filter(Objects::nonNull)
+            .map(o -> (Sub) o)
+            .collect(Collectors.toSet());
+    }
 
     public void reg(@NotNull Sub sub, @NotNull Class<?> type) {
         synchronized (lock) {
@@ -17,6 +42,12 @@ public class Bus {
                 .computeIfAbsent(type, c -> new ConcurrentSkipListSet<>());
             if (subs.contains(sub)) return;
             subs.add(sub);
+        }
+    }
+
+    public void regAll(@NotNull Object o, @NotNull Class<?> type) {
+        synchronized (lock) {
+            getSubs(o).forEach(s -> reg(s, type));
         }
     }
 
@@ -28,6 +59,12 @@ public class Bus {
             if (subs.isEmpty()) {
                 this.subs.remove(type);
             }
+        }
+    }
+
+    public void unregAll(@NotNull Object o, @NotNull Class<?> type) {
+        synchronized (lock) {
+            getSubs(o).forEach(s -> unreg(s, type));
         }
     }
 
