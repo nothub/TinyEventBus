f6c998735c5a2e339a996918977a2493d42fd41a
nothub <48992448+nothub@users.noreply.github.com>
2021-02-07 17:46:14 +0100

    unique test

--- src/test/java/cc/neckbeard/tinypubsub/tests/RegTest.java
+++ src/test/java/cc/neckbeard/tinypubsub/tests/RegTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Execution(ExecutionMode.CONCURRENT)
 class RegTest {
@@ -15,7 +16,7 @@ class RegTest {
     private Sub<BooleanEvent> fieldSub;
 
     @Test
-    void regAll() {
+    void regFields() {
         AtomicBoolean invoked = new AtomicBoolean(false);
         fieldSub = new Sub<>(BooleanEvent.class, e -> invoked.set(e.bool));
         Bus bus = new Bus();
@@ -25,6 +26,23 @@ class RegTest {
         bus.unregFields(this);
         bus.pub(new BooleanEvent(false));
         Assertions.assertTrue(invoked.get());
+    }
+
+    @Test
+    void regUnique() {
+        AtomicInteger invoked = new AtomicInteger(0);
+        Sub<BooleanEvent> sub1 = new Sub<>(BooleanEvent.class, e -> invoked.set(invoked.get() + 1));
+        Sub<BooleanEvent> sub2 = new Sub<>(BooleanEvent.class, e -> invoked.set(invoked.get() + 1));
+        Sub<BooleanEvent> sub3 = new Sub<>(BooleanEvent.class, e -> invoked.set(invoked.get() + 1));
+        Bus bus = new Bus();
+        bus.reg(sub1);
+        bus.reg(sub2);
+        bus.reg(sub2);
+        bus.reg(sub3);
+        bus.reg(sub3);
+        bus.reg(sub3);
+        bus.pub(new BooleanEvent(true));
+        Assertions.assertEquals(3, invoked.get());
     }
 
     @Test
