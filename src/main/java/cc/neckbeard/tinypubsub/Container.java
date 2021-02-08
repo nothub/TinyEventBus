52d69d4881d98484a20a96d1e4ddf37255434ad4
nothub <48992448+nothub@users.noreply.github.com>
2021-02-08 12:42:35 +0100

    0.2.2 skip handle invoke

--- src/main/java/cc/neckbeard/tinypubsub/Container.java
+++ src/main/java/cc/neckbeard/tinypubsub/Container.java
@@ -2,27 +2,27 @@ package cc.neckbeard.tinypubsub;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.invoke.MethodHandle;
+import java.lang.reflect.Method;
 
 class Container implements Comparable<Container> {
 
     final int prio;
     final Class<?> type;
-    final MethodHandle handle;
+    final Method method;
     final Object parent;
     final boolean statik;
 
-    Container(int prio, @NotNull Class<?> type, @NotNull MethodHandle handle, @NotNull Object parent, boolean statik) {
+    Container(int prio, @NotNull Class<?> type, Method method, @NotNull Object parent, boolean statik) {
         this.prio = prio;
         this.type = type;
-        this.handle = handle;
+        this.method = method;
         this.parent = parent;
         this.statik = statik;
     }
 
     @Override
     public int compareTo(@NotNull Container container) {
-        return container.prio == prio ? Integer.compare(container.handle.hashCode(), handle.hashCode()) : Integer.compare(container.prio, prio);
+        return container.prio == prio ? Integer.compare(container.method.hashCode(), method.hashCode()) : Integer.compare(container.prio, prio);
     }
 
 }
