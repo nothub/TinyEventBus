f1d18ca4b74a3f5f47c8c3ec45ddb62a39f76e06
nothub <48992448+nothub@users.noreply.github.com>
2021-02-12 15:36:01 +0100

    examples

--- src/test/java/cc/neckbeard/tinyeventbus/example/Main.java
+++
@@ -1,9 +0,0 @@
-package cc.neckbeard.tinyeventbus.example;
-
-public class Main {
-
-    public static void main(String[] args) {
-        new Example().run();
-    }
-
-}
---
+++ src/test/java/cc/neckbeard/tinyeventbus/example/RefExample.java
@@ -0,0 +1,73 @@
+package cc.neckbeard.tinyeventbus.example;
+
+import cc.neckbeard.tinyeventbus.Bus;
+import cc.neckbeard.tinyeventbus.Cancelable;
+import cc.neckbeard.tinyeventbus.Sub;
+
+class RefExample {
+
+    // TLDR: If TestEvent extends an implementation of Cancelable, called CancelableEvent in this example,
+    // the parameter of: Sub.of(TestEvent::cancel)
+    // will always be: Consumer<CancelableEvent>
+    // workaround: Sub.of(TestEvent::cancel, TestEvent.class);
+
+    // This reference lambda will have a generic type of CancelableEvent at invocation.
+    Sub<TestEvent> referenceLambdaSubBroken = Sub.of(TestEvent::cancel);
+
+    // To enforce a certain type, it can be supplied as additional parameter.
+    Sub<TestEvent> referenceLambdaSub = Sub.of(TestEvent::cancel, TestEvent.class);
+
+    void run() {
+
+        // create bus instance
+        Bus bus = new Bus();
+
+        // type breakage
+        bus.reg(referenceLambdaSubBroken);
+        final TestEvent evA = new TestEvent();
+        bus.pub(evA);
+        System.out.println(evA.canceled);
+        // prints: false
+
+        // workaround
+        bus.reg(referenceLambdaSub);
+        final TestEvent evB = new TestEvent();
+        bus.pub(evB);
+        System.out.println(evB.canceled);
+        // prints: true
+
+        // TODO: find a cleaner way of doing this
+
+    }
+
+    public abstract static class Event {
+    }
+
+    abstract static class CancelableEvent extends Event implements Cancelable {
+
+        boolean canceled;
+
+        CancelableEvent() {
+        }
+
+        public void cancel() {
+            this.canceled = true;
+        }
+
+        @Override
+        public boolean isCanceled() {
+            return canceled;
+        }
+
+    }
+
+    public static class TestEvent extends CancelableEvent {
+    }
+
+    static class Main {
+        public static void main(String[] args) {
+            new RefExample().run();
+        }
+    }
+
+}
